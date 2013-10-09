c0e337cc2615286df1bab8a7e6ccb19613afab18
galnegus <tobiashindersson@hotmail.com>
2013-10-09 19:23:05 +0200

    removed old slick code

--- src/gamedemo/component/RenderableComponent.java
+++
@@ -1,9 +0,0 @@
-package gamedemo.component;
-
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.state.StateBasedGame;
-
-public interface RenderableComponent {
-	void render(GameContainer gc, StateBasedGame sb, Graphics gr);
-}
